6fbedcf88d962e0fba2fd6a18724a81deec8dbac
AntonMakarkin <amakarkin27@gmail.com>
2021-03-25 13:37:52 +0300

    init prototype of app

---
+++ src/app.js
@@ -0,0 +1,104 @@
+import express from 'express'
+import path from 'path'
+import hbs from 'hbs'
+import { fileURLToPath } from 'url'
+import geocode from './utils/geocode.js'
+import forecast from './utils/forecast.js'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location (adding settings)
+app.set('view engine','hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve (load at first)
+app.use(express.static(publicDirectoryPath)) 
+
+app.get('/', (req, res) => { 
+    res.render('index', { //get view and converts it inti html
+        title: 'Weather',
+        name: 'Anton Makarkin'
+    }) 
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Anton Makarkin'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpMessage: 'Do you need help? Check this page and find an answer',
+        title: 'Help',
+        name: 'Anton Makarkin'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+    
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: [],
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Anton Makarkin',
+        errorMessage: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Anton Makarkin',
+        errorMessage: 'Page not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+import request from 'request';
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=f8626bd7c4b5b714577a053ee790f719&query=${latitude},${longitude}`;
+
+    request({ url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degress out`)
+        }
+    })
+}
+
+export default forecast
